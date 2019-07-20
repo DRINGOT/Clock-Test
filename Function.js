8fa2b7f232bac93538aa53e1a185c28b4ad7789d
DRINGOT <donovan.ringot@hotmail.fr>
2019-07-20 15:44:40 +0200

    creation and add 1st version jscript in Function.js

---
+++ Function.js
@@ -0,0 +1,7 @@
+function printTime(){var d= new Date ();
+    var hours=d.getHours();
+    var mins=d.getMinutes();
+    var secs=d.getSeconds();
+    var milli=d.getMilliseconds();
+document.getElementById('timedisplay').innerHTML=hours+":"+mins+":"+secs+"+milli;}
+setInterval(printTime,150);
