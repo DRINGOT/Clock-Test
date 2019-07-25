4551fb557a7293fe267cd57d084416768d82e6ba
DRINGOT <donovan.ringot@hotmail.fr>
2019-07-25 18:39:19 +0200

    add space in js

--- Function.js
+++ Function.js
@@ -1,7 +1,8 @@
-function printTime(){var d= new Date ();
-    var hours=d.getHours();
-    var mins=d.getMinutes();
-    var secs=d.getSeconds();
-    var milli=d.getMilliseconds();
-document.getElementById('timedisplay').innerHTML=hours+":"+mins+":"+secs+"+milli;}
+function printTime() {
+var d = new Date ();
+    var hours = d.getHours();
+    var mins = d.getMinutes();
+    var secs = d.getSeconds();
+    var milli = d.getMilliseconds();
+document.getElementById('timedisplay').innerHTML=hours+":"+mins+":"+secs+"+milli; }
 setInterval(printTime,150);
